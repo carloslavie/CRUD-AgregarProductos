ce7587304c3b92cea300207516b6ce94767156a0
carloslavie <carloslavie@gmail.com>
2021-04-10 11:57:49 -0300

    no arranca server

--- src/actions/productoActions.js
+++ src/actions/productoActions.js
@@ -1,7 +1,17 @@
 import { 
     AGREGAR_PRODUCTO,
     AGREGAR_PRODUCTO_EXITO,
-    AGREGAR_PRODUCTO_ERROR
+    AGREGAR_PRODUCTO_ERROR,
+    COMENZAR_DESCARGA_PRODUCTOS,
+    DESCARGA_PRODUCTOS_EXITO,
+    DESCARGA_PRODUCTOS_ERROR,
+    OBTENER_PRODUCTO_ELIMINAR,
+    PRODUCTO_ELIMINADO_EXITO,
+    PRODUCTO_ELIMINADO_ERROR,
+    OBTENER_PRODUCTO_EDITAR,
+    COMENZAR_EDICION_PRODUCTO,
+    PRODUCTO_EDITADO_EXITO,
+    PRODUCTO_EDITADO_ERROR
 } from '../types';
 
 import clienteAxios from '../config/axios';
@@ -43,7 +53,8 @@ export function crearNuevoProductoAction(producto){
 }
 
 const agregarProducto = () => ({
-    type: AGREGAR_PRODUCTO
+    type: AGREGAR_PRODUCTO,
+    payload: true
 })
 
 //Si el producto se guarda en la base de datos 
@@ -56,4 +67,106 @@ const agregarProductoExito = producto => ({
 const agregarProductoError = estado => ({
     type: AGREGAR_PRODUCTO_ERROR,
     payload: estado
+})
+
+
+//Funcion que descarga los productos de la base de datos
+export function obtenerProductosAction() {
+    return async(dispatch) => {
+        dispatch( descargarProductos() );
+
+        try {
+            const respuesta = await clienteAxios.get('/productos');
+            dispatch( descargaProductosExitosa(respuesta.data) );
+        } catch (error) {
+            dispatch( descargaProductosError() );
+        }
+    }
+}
+
+const descargarProductos = ()=> ({
+    type: COMENZAR_DESCARGA_PRODUCTOS,
+    payload: true
+})
+
+const descargaProductosExitosa = productos => ({
+    type: DESCARGA_PRODUCTOS_EXITO,
+    payload: productos
+})
+
+const descargaProductosError = ()=>({
+    type: DESCARGA_PRODUCTOS_ERROR,
+    payload: true
+})
+
+//Selecciona y elimina el producto
+export function borrarProductoAction(id) {
+    return async (dispatch) => {
+        dispatch( obtenerProductoEliminar(id) );
+        
+        try {
+            await clienteAxios.delete(`/productos/${id}`)
+            dispatch( eliminarProductoExito() );
+
+            //Si se elimina 
+            Swal.fire(
+                'Eliminado!',
+                'El producto se elimino correctamente.',
+                'success'
+              )
+        } catch (error) {
+            console.log(error);
+            dispatch( productoEliminarError() );
+        }
+    }
+}
+
+const obtenerProductoEliminar = id => ({
+    type: OBTENER_PRODUCTO_ELIMINAR,
+    payload:id
+});
+
+const eliminarProductoExito = () => ({
+    type: PRODUCTO_ELIMINADO_EXITO
+});
+
+const productoEliminarError = () => ({
+    type: PRODUCTO_ELIMINADO_ERROR,
+    payload: true
+});
+
+//Colocar producto en edicion
+export function obtenerProductoEditar(producto) {
+    return (dispatch) => {
+        dispatch( obtenerProductoEditarAction(producto) )
+    }
+} 
+
+const obtenerProductoEditarAction = producto => ({
+    type: OBTENER_PRODUCTO_EDITAR,
+    payload: producto
+})
+
+//Edita un registro en la API y state
+export function editarProductoAction(producto) {
+    return async (dispatch) => {
+        dispatch( editarProducto() );
+
+        try {
+            await clienteAxios.put(`/productos/${producto.id}`, producto);
+            
+            dispatch( editarProductoExito(producto) )
+        } catch (error) {
+            
+        }
+    }
+}
+
+const editarProducto = () => ({
+    type: COMENZAR_EDICION_PRODUCTO
+});
+
+const editarProductoExito = producto => ({
+    type:PRODUCTO_EDITADO_EXITO,
+    payload: producto
 })
---
+++ src/components/Producto.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import Swal from 'sweetalert2';
+//Redux
+import { useDispatch } from 'react-redux';
+import { borrarProductoAction, obtenerProductoEditar } from '../actions/productoActions'
+
+const Producto = ({producto}) => {
+
+    const { nombre, precio, id } = producto
+
+    const dispatch = useDispatch();
+    const history = useHistory();//Habilitar history para redireccion
+
+    //Confirmar si desea eliminar
+    const confirmarEliminarProducto = id =>{
+
+        //Preguntar al usuario
+        Swal.fire({
+            title: 'Estas seguro?',
+            text: "Un producto que se elimina no se puede volver a recuperar",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Sí, eliminar!',
+            cancelButtonText: 'Cancelar'
+          }).then((result) => {
+            if (result.isConfirmed) {
+                
+                //Pasarlo al action
+                dispatch( borrarProductoAction(id) );
+             
+            }
+          })
+    }
+
+    //Funcion que redirige de forma programada
+    const redireccionarEdicion = producto => {
+        dispatch( obtenerProductoEditar(producto) );
+        history.push(`/productos/editar/${producto.id}`);
+    }
+    return ( 
+        <tr>
+            <td>{nombre}</td>
+            <td><span className="font-weight-bold"> $ {precio} </span></td>
+            <td className="acciones">
+                <button
+                    type="button"
+                    onClick={()=>redireccionarEdicion(producto)}
+                    className="btn btn-primary mr-2">
+                    Editar
+                </button>
+                <button
+                    type= "button"
+                    className="btn btn-danger"
+                    onClick={() => confirmarEliminarProducto(id)}
+                > Eliminar </button>
+            </td>
+        </tr>
+     );
+}
+ 
+export default Producto;
